eb7d774eb2725c6fa4916a0e2927eab41f26219f
L + Ratio <who asked>
2022-11-30 14:59:12 +0700

    Them giao dich

---
+++ GiaoDich.java
@@ -0,0 +1,93 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class GiaoDich {
+    private String tenHang;
+    private String loaiHang;
+    private int soLuong;
+    private String loaiGiaoDich;
+    private double giaTongThe;
+    private Date thoiDiem;
+    private String diaDiem;
+
+    // constructor
+    public GiaoDich() {
+
+    }
+
+    public GiaoDich(String tenHang, String loaiHang, int soLuong, String loaiGiaoDich, double giaTongThe,
+            Date thoiDiem, String diaDiem) {
+        this.tenHang = tenHang;
+        this.loaiHang = loaiHang;
+        this.soLuong = soLuong;
+        this.loaiGiaoDich = loaiGiaoDich;
+        this.giaTongThe = giaTongThe;
+        this.thoiDiem = thoiDiem;
+        this.diaDiem = diaDiem;
+    }
+
+    // set
+    public void setTenHang(String tenHang) {
+        this.tenHang = tenHang;
+    }
+
+    public void setLoaiHang(String loaiHang) {
+        this.loaiHang = loaiHang;
+    }
+
+    public void setSoLuong(int soLuong) {
+        this.soLuong = soLuong;
+    }
+
+    public void setLoaiGiaoDich(String loaiGiaoDich) {
+        this.loaiGiaoDich = loaiGiaoDich;
+    }
+
+    public void setDiaDiem(String diaDiem) {
+        this.diaDiem = diaDiem;
+    }
+
+    public void setGiaTongThe(double giaTongThe) {
+        this.giaTongThe = giaTongThe;
+    }
+
+    public void setThoiDiem(Date thoiDiem) {
+        this.thoiDiem = thoiDiem;
+    }
+
+    // get
+    public String getTenHang() {
+        return this.tenHang;
+    }
+
+    public String getLoaiHang() {
+        return this.loaiHang;
+    }
+
+    public String getDiaDiem() {
+        return this.diaDiem;
+    }
+
+    public double getGiaTongThe() {
+        return this.giaTongThe;
+    }
+
+    public String getLoaiGiaoDich() {
+        return this.loaiGiaoDich;
+    }
+
+    public int getSoLuong() {
+        return this.soLuong;
+    }
+
+    public Date getThoiDiem() {
+        return this.thoiDiem;
+    }
+
+    public String toString() {
+        String result = String.format("%s,%s,%d,%s,%f,%s,%s\n", this.getTenHang(), this.getLoaiHang(),
+                this.getSoLuong(), this.getLoaiGiaoDich(), this.getGiaTongThe(), this.getThoiDiem().toString(),
+                this.getDiaDiem());
+        return result;
+    }
+}
